9f5b83746d95423be4ff42bb43f9b91cfbee5c0d
Glitch (glitch-hello-website) <none>
2021-12-10 21:46:36 +0000

    👮⛄️ Checkpoint ./index.html:11068696/23:31343558/9 ./shortcuts.html:31343558/297 ./shortcuts.js:31343558/9887 ./style.css:11068696/133:31343558/92 ./shortcuts.css:31343558/517

--- shortcuts.js
+++ shortcuts.js
@@ -1,3 +1,34 @@
+      var mac = document.getElementById("macParagraph");
+      mac.style.display = "none";
+      var chrome = document.getElementById("chromeParagraph");
+      chrome.style.display = "none";
+      var windows = document.getElementById("windowsParagraph");
+      windows.style.display = "none";
+
 function macFunction(){
-  
+        var intro = document.getElementById("macParagraph");
+        mac.style.display = "block";
+        var ovi = document.getElementById("chromeParagraph");
+        chrome.style.display = "none";
+        var mcDavid = document.getElementById("windowsParagraph");
+        windows.style.display = "none";
 }
+
+function chromeFunction(){
+        var intro = document.getElementById("macParagraph");
+        mac.style.display = "none";
+        var ovi = document.getElementById("chromeParagraph");
+        chrome.style.display = "block";
+        var mcDavid = document.getElementById("windowsParagraph");
+        windows.style.display = "none";
+}
+
+function windowsFunction(){
+        var intro = document.getElementById("macParagraph");
+        mac.style.display = "none";
+        var ovi = document.getElementById("chromeParagraph");
+        chrome.style.display = "none";
+        var mcDavid = document.getElementById("windowsParagraph");
+        windows.style.display = "block";
+}
+
