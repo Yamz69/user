c721ac827d23337abce383c18f451f11b686ea00
Glitch (turk-and-yamz) <none>
2021-12-16 15:30:19 +0000

    ⏳😀 Checkpoint ./ways/theSpheroEDUWay.html:31343558/17 ./unblockedGamesTest.js:31343558/700 ./unblockedGamesSlopeTest.html:31343558/6 ./unblockedGamesHomeTest.html:31343558/492

---
+++ unblockedGamesTest.js
@@ -0,0 +1,12 @@
+var slope = document.getElementById("slopeGame");
+slope.style.display = "none";
+
+var soccerPhysics = document.getElementById("soccerPhysicsGame");
+soccerPhysics.style.display = "none";
+
+var billy = document.getElementById("billyGame");
+billy.style.display = "none";
+
+function slopeGame() {
+  document.getElementById("slopeGame1").requestFullscreen();
+}
