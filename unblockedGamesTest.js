1c5823aa2b6f3b012d8e08724a2554dfdeedc888
Glitch (turk-and-yamz) <none>
2021-12-16 16:06:32 +0000

    🎃🌋 Checkpoint ./games/soccerPhysics.html:31343558/162 ./unblockedGamesTest.js:31343558/254 ./unblockedGamesHomeTest.html:31343558/46 ./games/billy.html:31343558/19 ./games/slope.html:31343558/19

--- unblockedGamesTest.js
+++ unblockedGamesTest.js
@@ -10,3 +10,9 @@ billy.style.display = "none";
 function slopeGame() {
   document.getElementById("slopeGame1").requestFullscreen();
 }
+function soccerPhysicsGame() {
+  document.getElementById("soccerPhysicsGame1").requestFullscreen();
+}
+function billyGame() {
+  document.getElementById("billyGame1").requestFullscreen();
+}
